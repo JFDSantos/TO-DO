5758e36ca226f2322fde2a94a9de326dbcb97e6f
Jeferson.Ferreira <jeeh71ferreira@gmail.com>
2024-12-01 15:48:35 -0300

    Subindo CI/CD

--- app/redis_cache.py
+++ app/redis_cache.py
@@ -9,8 +9,14 @@ REDIS_HOST = parsed_redis_url.hostname or "localhost"
 REDIS_PORT = parsed_redis_url.port or 6379
 
 def get_redis():
-    return redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True, ssl=True)
-
+    return redis.StrictRedis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=0,
+        password=os.getenv("REDIS_PASSWORD"),
+        decode_responses=True,
+        ssl=True  # Habilita SSL para a comunicação segura
+    )
 
 def store_token_in_cache(username: str, token: str, expiration: int = 3600):
     redis_client = get_redis()
@@ -19,6 +25,3 @@ def store_token_in_cache(username: str, token: str, expiration: int = 3600):
 async def get_token_from_cache(username: str):
     redis_client = get_redis()
     return redis_client.get(f"token:{username}")
-
-
-
